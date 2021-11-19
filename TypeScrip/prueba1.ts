736918586c35bfb8e892ab164c9c82eb63962b48
Pereyra Hugo <pereyrahugor@gmail.com>
2021-11-19 11:26:26 -0300

    incorporación de json y ts

---
+++ TypeScrip/prueba1.ts
@@ -0,0 +1,15 @@
+let a: number; //* Inferencia explícita
+let b: string; //* Inferencia explícita
+let c=101;     //* Inferencia implícita
+
+//*c="one"
+
+let recursos: ["memoria","disco","procesador"];
+
+let recursos2: ["memoria","disco",100];
+
+let recursos3: string[] = ["memoria","disco","procesador"];
+
+//*recursos3= ["memory", "disc", 101];
+
+
